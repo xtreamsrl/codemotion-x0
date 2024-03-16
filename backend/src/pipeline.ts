3941a9d170fc724fe137606bca9bd61895d60d6f
zoncrd <riccardo.zoncada@xtreamers.io>
2024-03-16 12:31:40 +0100

    feat: add simple design step

--- backend/src/pipeline.ts
+++ backend/src/pipeline.ts
@@ -1,6 +1,10 @@
 import { PipelineInputs, PipelineOutput } from './utils/types';
+import { designStep } from './steps/designStep';
 
 export async function pipeline(inputs: PipelineInputs): Promise<PipelineOutput> {
+  const designOutput = await designStep(inputs);
+  console.log(designOutput);
+
   return {
     code: 'console.log("Hello, World!")',
     path: 'path/to/file.tsx',
---
+++ backend/src/steps/designStep.ts
@@ -0,0 +1,35 @@
+import { PipelineInputs } from '../utils/types';
+import { designSystemPrompt, designUserPromptTemplate } from '../prompts/designNewComponent';
+import OpenAI from 'openai';
+import COMPONENT_METADATA from '../data/components.json';
+
+export async function designStep(inputs: PipelineInputs): Promise<string> {
+  // Retrieve context
+  const componentList = COMPONENT_METADATA.map(
+    (component) => `${component.name} : ${component.description}`,
+  ).join('\n');
+
+  // Prepare prompt messages
+  const designUserPrompt = designUserPromptTemplate.format({
+    userDescription: inputs.userDescription,
+    libraryComponents: componentList,
+  });
+  const messages = [
+    { role: 'system' as const, content: designSystemPrompt },
+    { role: 'user' as const, content: designUserPrompt },
+  ];
+
+  // Invoke llm
+  const openaiClient = new OpenAI();
+  const chatCompletion = await openaiClient.chat.completions.create({
+    model: 'gpt-4-turbo-preview',
+    messages,
+  });
+
+  // Parse/format output
+  const designOutput = chatCompletion.choices[0].message.content;
+  if (!designOutput) {
+    throw new Error('Failed to generate design output');
+  }
+  return designOutput;
+}
